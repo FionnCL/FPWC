1f84bd63a74e25474736ae6fb3c05b080b3ec96d
FionnCL <103070351+FionnCL@users.noreply.github.com>
2023-10-19 17:53:48 +0100

    EVERYTHING BUT TERMINATE WORKS

--- Exercise2/src/Ex2.hs
+++ Exercise2/src/Ex2.hs
@@ -1,6 +1,7 @@
 module Ex2 where
 import Data.Maybe
 import Debug.Trace
+import Text.Printf
 
 debug = flip trace
 
@@ -58,20 +59,21 @@ f4 :: [Maybe Int] -> (Int, [Maybe Int])
 --    |   53   |    mul    | stop@ 6  | skip    |
 --    |   35   |    mul    | stop@ 3  | 1       |
 --    -------------------------------------------
+-- -1 = term, -2 = skip, any other value will be set as the number it must be
 f4 (Just x:xs)
-	| x == 60	= (addRecurse (take 6 xs), (drop 6 xs))
-	| x == 32	= (addRecurse (take 6 xs), (drop 6 xs))
-	| x == 41	= (addRecurse (take 6 xs), (drop 6 xs))
-	| x == 71	= (addRecurse (findStop xs 6), (drop (length (findStop xs 6)) xs))
-	| x == 40	= (addRecurse (findStop xs 3), (drop (length (findStop xs 3)) xs))
-	| x == 68	= (addRecurse (findStop xs 4), (drop (length (findStop xs 4)) xs))
-	| x == 73	= (mulRecurse (take 5 xs), (drop 5 xs))
-	| x == 57	= (mulRecurse (take 5 xs), (drop 5 xs))
-	| x == 52	= (mulRecurse (take 6 xs), (drop 6 xs))
-	| x == 43	= (mulRecurse (findStop xs 4), (drop (length (findStop xs 4)) xs))
-	| x == 53	= (mulRecurse (findStop xs 6), (drop (length (findStop xs 6)) xs))
-	| x == 35	= (mulRecurse (findStop xs 3), (drop (length (findStop xs 3)) xs))
-	| otherwise = (x,xs)
+  | x == 60 = (addRecurse (take 6 xs) (-1), (drop 6 xs)) `debug` "OPCODE: 60"
+  | x == 32 = (addRecurse (take 6 xs) (-2), (drop 6 xs))
+  | x == 41 = (addRecurse (take 6 xs) 9, (drop 6 xs)) `debug` "OPCODE: 41"
+  | x == 71 = (addRecurse (findStop xs 6) (-1), (drop (length (findStop xs 6)) xs))
+  | x == 40 = (addRecurse (findStop xs 3) (-2), (drop (length (findStop xs 3)) xs))
+  | x == 68 = (addRecurse (findStop xs 4) 6, (drop (length (findStop xs 4)) xs))
+  | x == 73 = (mulRecurse (take 5 xs) (-1), (drop 5 xs))
+  | x == 57 = (mulRecurse (take 5 xs) (-2), (drop 5 xs))
+  | x == 52 = (mulRecurse (take 6 xs) 7, (drop 6 xs))
+  | x == 43 = (mulRecurse (findStop xs 4) (-1), (drop (length (findStop xs 4)) xs))
+  | x == 53 = (mulRecurse (findStop xs 6) (-2), (drop (length (findStop xs 6)) xs))
+  | x == 35 = (mulRecurse (findStop xs 3) 1, (drop (length (findStop xs 3)) xs))
+  | otherwise = (x,xs)
 
 -- NOTE: rest means "rest of the integer list"
 -- An opcode defines three things: 
@@ -80,7 +82,7 @@ f4 (Just x:xs)
 --          a.  fixed N means operate on the next N numbers.
 --          b.  stop @K means stop when value K is encountered(not considered an operand).
 --    3.  HOW TO HANDLE CORUPTTED VALUES(stop/skip/def-value)
---          a. stop = finish calculation
+--          a. stop(term/terminate) = finish calculation
 --          b. skip = move to next list element
 --          c. N = treat corrupted element as having value N
 --    This gives 12 combinations of things to do.
@@ -104,23 +106,30 @@ f5 mis = undefined
 -- add extra material below here
 -- e.g.,  helper functions, test values, etc. ...
 -- ADDITION HELPERS
-addRecurse :: [Maybe Int] -> Int
-addRecurse mis = case mis of
-	y : ys ->	(fromJust y) + (addRecurse ys)
-	isNothing -> case type of
-		"term" -> I DON'T KNOW
-		"skip" -> addRecurse ys
-		_ -> type + (addRecurse ys)
+-- The second argument will be denoted as: -1 for term, -2 for skip, and any other value will become x
+addRecurse :: [Maybe Int] -> Int -> Int
+addRecurse [] _ = 0
+addRecurse (x:xs) nothingCase
+  | not(isNothing x) = (fromJust x) + (addRecurse xs nothingCase) `debug` "Recursed Normally"
+  | isNothing x = 
+    case nothingCase of
+      (-1) -> 0 `debug` "-1"
+      (-2) -> addRecurse xs nothingCase `debug` "-2"
+      otherwise -> (nothingCase) + (addRecurse xs nothingCase) `debug` "Def-val"
 
 findStop :: [Maybe Int] -> Int -> [Maybe Int]
 findStop (x:xs) stopNum
-	| x /= (Just stopNum) = x : findStop xs stopNum
-	| otherwise = []
+  | x /= (Just stopNum) = x : findStop xs stopNum
+  | otherwise = []
 
-mulRecurse :: [Maybe Int] -> Int
-mulRecurse mis = case mis of
-	y : ys -> (fromJust y) * (mulRecurse ys)
-	isNothing -> case type of
-		"term" -> I DON'T KNOW
-		"skip" -> addRecurse ys
-		_ -> type * (addRecurse ys)
+-- findTerminate :: [Maybe Int] -> [Maybe Int]
+
+mulRecurse :: [Maybe Int] -> Int-> Int
+mulRecurse [] _ = 1
+mulRecurse (x:xs) nothingCase
+  | isJust x = (fromJust x) + (addRecurse xs nothingCase)
+  | otherwise = 
+    case nothingCase of
+      (-1) -> 1 `debug` "-1"
+      (-2) -> mulRecurse xs nothingCase `debug` "-2"
+      otherwise -> (nothingCase) * (mulRecurse xs nothingCase) `debug` "Def-Val"
