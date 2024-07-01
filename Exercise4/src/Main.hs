3c5c5164c5c9792c1c622ffa459dcc3b457407f2
FionnCL <103070351+FionnCL@users.noreply.github.com>
2024-07-01 22:11:44 +0100

    Update Main.hs

--- Exercise4/src/Main.hs
+++ Exercise4/src/Main.hs
@@ -33,36 +33,3 @@ main = do
       writeList ho result
       hClose h
       hClose ho
-      
--- main 
---   = putStrLn $ unlines
---       [ "Running Exercise4."
---       , "You should modify this program as follows:"
---       , "It should open and read a file called `input.dat`"
---       , "This file contains a number of Ints, each on its own line"
---       , "There is a list of functions defined in variable `ops` in Ex4.hs"
---       , "Your `ops` list has length N="++show (length ops)
---       , "The 1st function is applied to the 1st number read,"
---       , "The 2nd function is applied to the 2nd number read,"
---       , " proceed like this until:"
---       , "The Nth function is applied to the Nth number read."
---       , "Processing moves back to the 1st function in the list, so..."
---       , "The 1st function is applied to the (N+1)th number read,"
---       , "The 2st function is applied to the (N+2)th number read,"
---       , "and so on..."
---       , "Continue until all input numbers have been processed."
---       , "The resulting numbers should be written, one per line, to `output.dat`"
-
---       , "\"mdeval\" Tests"
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (MulBy (VarNm \"XD\") (Value 3)) = " ++ show (mdeval d (MulBy (VarNm "XD") (Value 3.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (Divide (Value 1) (Value 3)) = " ++ show (mdeval d (Divide (Value 1.0) (Value 3.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (Divide (Value 1) (Value 0)) = " ++ show (mdeval d (Divide (Value 1.0) (Value 0.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (AddInv (Value 1)) = " ++ show (mdeval d (AddInv (Value 1.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (AddInv (Value -1)) = " ++ show (mdeval d (AddInv (Value (-1.0))))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (Not (Value 0)) = " ++ show (mdeval d (Not (Value 0.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (Not (Value 1)) = " ++ show (mdeval d (Not (Value 1.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (Dfrnt (Value 1) (Value 3)) = " ++ show (mdeval d (Dfrnt (Value 1.0) (Value 3.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (Dfrnt (Value 1) (Value 1)) = " ++ show (mdeval d (Dfrnt (Value 1.0) (Value 1.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (IsNil (Value 0.0)) = " ++ show (mdeval d (IsNil (Value 0.0)))
---       , "mdeval [(\"XX\",1),(\"XD\",20),(\"DD\", 41)] (IsNil (Value 1.0)) = " ++ show (mdeval d (IsNil (Value 1.0)))
---       ]
